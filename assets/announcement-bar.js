e6aef907ae281dfe0589920722619a151a9e3a0d
ricard0g <ricardoguzman444@gmail.com>
2024-12-07 20:35:19 +0100

    Announcement bar functioning with the autoplay logic, also buttons trigger the correct function. Styles apply a 'fade-in' effect from left to right correctly

---
+++ assets/announcement-bar.js
@@ -0,0 +1,30 @@
+// Autoplay
+setInterval(() => {
+  const slides = document.querySelector('[data-carousel]').querySelector('[data-slides]');
+
+  const activeSlide = slides.querySelector('[data-active]');
+  let newIndex = [...slides.children].indexOf(activeSlide) + 1;
+  if (newIndex < 0) newIndex = slides.children.length - 1;
+  if (newIndex >= slides.children.length) newIndex = 0;
+
+  slides.children[newIndex].dataset.active = true;
+  delete activeSlide.dataset.active;
+}, 3000);
+
+// Button's Functionality
+const buttons = document.querySelectorAll('[data-carousel-button]');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const offset = button.dataset.carouselButton === 'next' ? 1 : -1;
+    const slides = button.closest('[data-carousel]').querySelector('[data-slides]');
+
+    const activeSlide = slides.querySelector('[data-active]');
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
+});
