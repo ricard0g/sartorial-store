8d72a4c22dede27540b01e0eacf26910e68dc598
ricard0g <ricardoguzman444@gmail.com>
2024-12-12 20:52:58 +0100

    Mega-menu shows image for a collection, iterates correctly accross the sublinks and now we only need the other image to be correctly displayed

--- assets/navbar.js
+++ assets/navbar.js
@@ -4,5 +4,16 @@ navLinks.forEach(link => {
     link.addEventListener('mouseover', (e) => {
         const megaMenu = e.target.nextElementSibling;
         megaMenu.classList.add('show');
+        megaMenu.addEventListener('mouseover', (e) => {
+            megaMenu.classList.add('show')
+        });
+        megaMenu.addEventListener('mouseout', (e) => {
+            megaMenu.classList.remove('show')
+        })
+    });
+
+    link.addEventListener('mouseout', (e) => {
+       const megaMenu = e.target.nextElementSibling;
+       megaMenu.classList.remove('show');
     })
 })
