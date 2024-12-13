d846621eb9793b4c0e15c11164614bd6454b2861
ricard0g <ricardoguzman444@gmail.com>
2024-12-13 15:26:59 +0100

    Mega-menu functionality ready, still have to style it. Remember to style each link to each section with border-bottom and not text-decoration: 'underline'

--- assets/navbar.js
+++ assets/navbar.js
@@ -1,19 +1,33 @@
 const navLinks = document.querySelectorAll('.nav-link');
 
-navLinks.forEach(link => {
-    link.addEventListener('mouseover', (e) => {
-        const megaMenu = e.target.nextElementSibling;
-        megaMenu.classList.add('show');
-        megaMenu.addEventListener('mouseover', (e) => {
-            megaMenu.classList.add('show')
-        });
-        megaMenu.addEventListener('mouseout', (e) => {
-            megaMenu.classList.remove('show')
-        })
+navLinks.forEach((link) => {
+  link.addEventListener('mouseover', (e) => {
+    link.classList.add('underline');
+    const megaMenu = e.target.nextElementSibling;
+    megaMenu.style.display = 'block';
+    setTimeout(() => {
+      megaMenu.classList.add('show');
+    }, 100);
+    megaMenu.addEventListener('mouseover', (e) => {
+      megaMenu.style.display = 'block';
+      megaMenu.classList.add('show');
     });
+    megaMenu.addEventListener('mouseout', (e) => {
+      megaMenu.classList.remove('show');
+      megaMenu.style.display = 'none';
+      setTimeout(() => {
+        if (megaMenu.style.display === "none") {
+            link.classList.remove('underline');
+        }
+      }, 100)
+    });
+  });
 
-    link.addEventListener('mouseout', (e) => {
-       const megaMenu = e.target.nextElementSibling;
-       megaMenu.classList.remove('show');
-    })
-})
+  link.addEventListener('mouseout', (e) => {
+    const megaMenu = e.target.nextElementSibling;
+    if (!megaMenu.classList.contains('show')) {
+      megaMenu.classList.remove('show');
+      megaMenu.style.display = 'none';
+    }
+  });
+});
