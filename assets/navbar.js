dceb5c9beeae89c6c5595f3a302babc9a189fcd1
ricard0g <ricardoguzman444@gmail.com>
2024-12-15 14:56:32 +0100

    Saving

--- assets/navbar.js
+++ assets/navbar.js
@@ -1,33 +1,21 @@
 const navLinks = document.querySelectorAll('.nav-link');
+const subNavbarWrapper = document.querySelector('.subnavbar-wrapper');
 
 navLinks.forEach((link) => {
-  link.addEventListener('mouseover', (e) => {
-    link.classList.add('underline');
-    const megaMenu = e.target.nextElementSibling;
-    megaMenu.style.display = 'block';
-    setTimeout(() => {
-      megaMenu.classList.add('show');
-    }, 100);
-    megaMenu.addEventListener('mouseover', (e) => {
-      megaMenu.style.display = 'block';
-      megaMenu.classList.add('show');
+    const megaMenu = link.nextElementSibling;
+    link.addEventListener('mouseover', (e) => {
+        link.classList.toggle('underline');
+        megaMenu.style.display = 'block';
+        megaMenu.classList.add('show');
     });
-    megaMenu.addEventListener('mouseout', (e) => {
-      megaMenu.classList.remove('show');
-      megaMenu.style.display = 'none';
-      setTimeout(() => {
-        if (megaMenu.style.display === "none") {
-            link.classList.remove('underline');
-        }
-      }, 100)
-    });
-  });
 
-  link.addEventListener('mouseout', (e) => {
-    const megaMenu = e.target.nextElementSibling;
-    if (!megaMenu.classList.contains('show')) {
-      megaMenu.classList.remove('show');
-      megaMenu.style.display = 'none';
-    }
-  });
+    link.addEventListener('mouseleave', () => {
+        link.classList.toggle('underline');
+        megaMenu.classList.remove('show');
+        megaMenu.style.display = 'none';
+    })
 });
+
+subNavbarWrapper.addEventListener('mouseover', () => {
+    megaMenu.classList.add('show');
+})
