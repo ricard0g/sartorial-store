f057061d1059f661bc8b7f1de44bc2529c177d7e
ricard0g <ricardoguzman444@gmail.com>
2025-03-11 19:44:03 +0100

    feat(facets): FacetFiltersForm web component is complete with all methods and explanations in comments

--- assets/facets.js
+++ assets/facets.js
@@ -32,7 +32,7 @@ class FacetFiltersForm extends HTMLElement {
         });
     }
 
-    static renderPage(searchParams, facetForm, updateURLHash = true) {
+    static renderPage(searchParams, event, updateURLHash = true) {
         FacetFiltersForm.searchParamsPrev = searchParams;
         const sections = FacetFiltersForm.getSections();
         const countContainer = document.getElementById('ProductCount');
@@ -112,13 +112,144 @@ class FacetFiltersForm extends HTMLElement {
             }
         });
 
+        // This function will check if the element that was clicked is the same as the one that was returned from the server
+        // If it is, we will return true and the element will be rendered
+        // If it is not, we will return false and the element will not be rendered
+        // It's a function that checks the interacted element with the one that was returned from the server
         const matchesId = (el) => {
             const jsFilter = event ? event.target.closest('.js-filter') : undefined;
             return jsFilter ? el.id === jsFilter.id : false;
         };
 
+        // FacetsToRender is an array containing all the facets that are returned from the server except the one that was clicked
         const facetsToRender = Array.from(facetDetailsElementsFromFetch).filter((el) => !matchesId(el));
+        // Counts are the number right next to the actual filter. "Counting" the number of items available for that filter
+        // In this case we are looking for the element that has the same id as the one that was clicked
         const countsToRender = Array.from(facetDetailsElementsFromFetch).find(matchesId);
+
+        // For each Facet to Render we will check if the element already exists in the DOM
+        // If it does, we will update the innerHTML of the element with the one that was returned from the server
+        // If it doesn't, we will insert the new element after the previous one
+        facetsToRender.forEach((elToRender, index) => {
+            const currentElement = document.getElementById(elToRender.id);
+            // Element already rendered in the DOM so just update the innerHTML
+            if (currentElement) {
+                document.getElementById(elToRender.id).innerHTML = elToRender.innerHTML;
+            } else {
+                if (index > 0) {
+                    const { className: previousElementClassName, id: previousElementId } = facetsToRender[index - 1];
+                    // Same facet type (eg vertical or drawer/mobile)
+                    if (elToRender.className === previousElementClassName) {
+                        // Insert the new element after the previous one
+                        document.getElementById(previousElementId).after(elToRender);
+                        return;
+                    }
+                }
+            }
+
+            if (elToRender.parentElement) {
+                document.querySelector(`#${elToRender.parentElement.id} .js-filter`).before(elToRender);
+            }
+        });
+
+        FacetFiltersForm.renderActiveFacets(parsedHTML);
+        FacetFiltersForm.renderAdditionalElements(parsedHTML);
+
+        // Render the counts for the facets that were returned from the server, if there are any changes
+        if (countsToRender) {
+            const closestJSFilterId = event.target.closest('.js-filter').id;
+
+            if (closestJSFilterId) {
+                FacetFiltersForm.renderCounts(countsToRender, event.target.closest('.js-filter'));
+                FacetFiltersForm.renderMobileCounts(countsToRender, document.getElementById(closestJSFilterId));
+
+                const newFacetDetailsElement = document.getElementById(closestJSFilterId);
+                const newElementSelector = newFacetDetailsElement.classList.contains('mobile-facets__details')
+                    ? '.mobile-facets__close-button'
+                    : '.facets__summary';
+                const newElementToActive = newFacetDetailsElement.querySelector(newElementSelector);
+
+                const isTextInput = event.target.getAttribute('type') === 'text';
+
+                if (newElementToActive && !isTextInput) newElementToActive.focus();
+            }
+        }
+    }
+
+    // Method to handle the selected filters on the UI. Here we manage those filter "tags" that are shown on the top of the page
+    // Here we're updating the entire section that contains the active filter tags applied to the collection grid
+    static renderActiveFacets(html) {
+        const activeFacetElementSelectors = ['.active-facets-mobile', '.active-facets-desktop'];
+
+        activeFacetElementSelectors.forEach((selector) => {
+            // Here we have the active facets that are returned from the server and then replace the active facets section that's currently in the DOM with those returned from the server
+            const activeFacetsElement = html.querySelector(selector);
+            if (!activeFacetsElement) return;
+            document.querySelector(selector).innerHTML = activeFacetsElement.innerHTML;
+        });
+
+        FacetFiltersForm.toggleActiveFacets(false);
+    }
+
+    static renderAdditionalElements(html) {
+        const mobileElementSelectors = ['.mobile-facets__open', '.mobile-facets__count', '.sorting'];
+
+        mobileElementSelectors.forEach((selector) => {
+            if (!html.querySelector(selector)) return;
+            // Here we have the active facets that are returned from the server and then replace the active facets section that's currently in the DOM with those returned from the server
+            document.querySelector(selector).innerHTML = html.querySelector(selector).innerHTML;
+        });
+
+        // bindEvents will reattach all event listeners to the mobile drawer after its HTML content has been updated
+        // Important line for re-initializing the mobile drawer functionality
+        document.getElementById('FacetFiltersFormMobile').closest('menu-drawer').bindEvents();
+    }
+
+    static renderCounts(source, target) {
+        const targetSummary = target.querySelector('.facets__summary');
+        const sourceSummary = source.querySelector('.facets__summary');
+
+        if (sourceSummary && targetSummary) {
+            targetSummary.outerHTML = sourceSummary.outerHTML;
+        }
+
+        const targetHeaderElement = target.querySelector('.facets__header');
+        const sourceHeaderElement = source.querySelector('.facets__header');
+
+        if (sourceHeaderElement && targetHeaderElement) {
+            targetHeaderElement.outerHTML = sourceHeaderElement.outerHTML;
+        }
+
+        const targetWrapElement = target.querySelector('.facets-wrap');
+        const sourceWrapElement = source.querySelector('.facets-wrap');
+
+        if (sourceWrapElement && targetWrapElement) {
+            const isShowingMore = Boolean(targetWrapElement.querySelector('show-more-button .label-show-more.hidden'));
+            if (isShowingMore) {
+                sourceWrapElement
+                    .querySelectorAll('.facets__item.hidden')
+                    .forEach((hiddenItem) => hiddenItem.classList.replace('hidden', 'show-more-item'));
+            }
+
+            targetWrapElement.outerHTML = sourceWrapElement.outerHTML;
+        }
+    }
+
+    static renderMobileCounts(source, target) {
+        const targetFacetList = target.querySelector('.mobile-facets__list');
+        const sourceFacetList = source.querySelector('.mobile-facets__list');
+
+        if (sourceFacetList && targetFacetList) {
+            targetFacetList.outerHTML = sourceFacetList.outerHTML;
+        }
+    }
+
+    static updateURLHash(searchParams) {
+        history.pushState(
+            { searchParams },
+            '',
+            `${window.location.pathname}${searchParams && '?'.concat(searchParams)}`
+        );
     }
 
     // Method to get sections that will be updated maybe using Section Rendering API
@@ -129,4 +260,56 @@ class FacetFiltersForm extends HTMLElement {
             },
         ];
     }
+
+    createSearchParams(form) {
+        const formData = new FormData(form);
+        return new URLSearchParams(formData).toString();
+    }
+
+    onSubmitForm(searchParams, e) {
+        FacetFiltersForm.renderPage(searchParams, e);
+    }
+
+    onSubmitHandler(e) {
+        e.preventDefault();
+        const sortFilterForms = document.querySelectorAll('facet-filters-form form');
+        if (e.target.className == 'mobile-facets__checkbox') {
+            const searchParams = this.createSearchParams(e.target.closest('form'));
+            this.onSubmitForm(searchParams, e);
+        } else {
+            const forms = [];
+            const isMobile = e.target.closest('form').id === 'FacetFiltersFormMobile';
+
+            sortFilterForms.forEach((form) => {
+                if (!isMobile) {
+                    if (
+                        form.id === 'FacetSortForm' ||
+                        form.id === 'FacetFiltersForm' ||
+                        form.id === 'FacetSortDrawerForm'
+                    ) {
+                        forms.push(this.createSearchParams(form));
+                    }
+                } else if (form.id === 'FacetFiltersFormMobile') {
+                    forms.push(this.createSearchParams(form));
+                }
+            });
+            this.onSubmitForm(forms.join('&'), e);
+        }
+    }
+
+    onActiveFilterClick(e) {
+        e.preventDefault();
+        FacetFiltersForm.toggleActiveFacets();
+        const url =
+            e.currentTarget.href.indexOf('?') === -1
+                ? ''
+                : e.currentTarget.href.slice(e.currentTarget.href.indexOf('?') + 1);
+        FacetFiltersForm.renderPage(url);
+    }
 }
+
+FacetFiltersForm.filterData = [];
+FacetFiltersForm.searchParamsInitial = window.location.search.slice(1);
+FacetFiltersForm.searchParamsPrev = window.location.search.slice(1);
+customElements.define('facet-filters-form', FacetFiltersForm);
+FacetFiltersForm.setListeners();
