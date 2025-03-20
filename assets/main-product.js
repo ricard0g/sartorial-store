59b5077ea08de6d52a65f0d64d60323cfabdab66
ricard0g <ricardoguzman444@gmail.com>
2025-03-20 13:32:58 +0100

    feat(product-sliders): add jquery to project only on 'product' template, create vertical slider and big slider, both connected. Still have to style button son main one

--- assets/main-product.js
+++ assets/main-product.js
@@ -1,14 +1,27 @@
-const elemNav = document.querySelector('.main-product__carousel-nav');
-var flktyNav = new Flickity(elemNav, {
-    asNavFor: '.main-product__carousel-main',
-    contain: true,
+var $carousel = $('.main-product__carousel-main').flickity({
+    imagesLoaded: true,
+    cellAlign: 'center',
     pageDots: false,
-    draggable: false,
-    cellAlign: 'left',
 });
 
-const elem = document.querySelector('.main-product__carousel-main');
-var flkty = new Flickity(elem, {
-    contain: true,
-    cellAlign: 'left',
+var $carouselNav = $('.main-product__carousel-nav');
+var $carouselNavCells = $carouselNav.find('.main-product__carousel-nav-cell');
+
+$carouselNav.on('click', '.main-product__carousel-nav-cell', function (e) {
+    var index = $(e.currentTarget).index();
+    $carousel.flickity('select', index);
+});
+
+var flkty = $carousel.data('flickity');
+var navTop = $carouselNav.position().top;
+var navCellHeight = $carouselNavCells.height();
+var navHeight = $carouselNav.height();
+
+$carousel.on('select.flickity', function () {
+    $carouselNav.find('.is-nav-selected').removeClass('is-nav-selected');
+    var $selected = $carouselNavCells.eq(flkty.selectedIndex).addClass('is-nav-selected');
+    var scrollY = $selected.position().top + $carouselNav.scrollTop() - (navHeight + navCellHeight) / 2;
+    $carouselNav.animate({
+        scrollTop: scrollY,
+    });
 });
