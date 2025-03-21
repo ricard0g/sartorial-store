75f1f3d0e0097c65865b7338c5a787a0e3533da2
ricard0g <ricardoguzman444@gmail.com>
2025-03-21 17:12:53 +0100

    fix(main-product): change scripts order and jquery script loading on theme.liquid in order to solve the sometimes broken slider on first render

--- assets/main-product.js
+++ assets/main-product.js
@@ -1,27 +1,29 @@
-var $carousel = $('.main-product__carousel-main').flickity({
-    imagesLoaded: true,
-    cellAlign: 'center',
-    pageDots: false,
-});
+document.addEventListener('DOMContentLoaded', function () {
+    var $carousel = $('.main-product__carousel-main').flickity({
+        imagesLoaded: true,
+        cellAlign: 'center',
+        pageDots: false,
+    });
 
-var $carouselNav = $('.main-product__carousel-nav');
-var $carouselNavCells = $carouselNav.find('.main-product__carousel-nav-cell');
+    var $carouselNav = $('.main-product__carousel-nav');
+    var $carouselNavCells = $carouselNav.find('.main-product__carousel-nav-cell');
 
-$carouselNav.on('click', '.main-product__carousel-nav-cell', function (e) {
-    var index = $(e.currentTarget).index();
-    $carousel.flickity('select', index);
-});
+    $carouselNav.on('click', '.main-product__carousel-nav-cell', function (e) {
+        var index = $(e.currentTarget).index();
+        $carousel.flickity('select', index);
+    });
 
-var flkty = $carousel.data('flickity');
-var navTop = $carouselNav.position().top;
-var navCellHeight = $carouselNavCells.height();
-var navHeight = $carouselNav.height();
+    var flkty = $carousel.data('flickity');
+    var navTop = $carouselNav.position().top;
+    var navCellHeight = $carouselNavCells.height();
+    var navHeight = $carouselNav.height();
 
-$carousel.on('select.flickity', function () {
-    $carouselNav.find('.is-nav-selected').removeClass('is-nav-selected');
-    var $selected = $carouselNavCells.eq(flkty.selectedIndex).addClass('is-nav-selected');
-    var scrollY = $selected.position().top + $carouselNav.scrollTop() - (navHeight + navCellHeight) / 2;
-    $carouselNav.animate({
-        scrollTop: scrollY,
+    $carousel.on('select.flickity', function () {
+        $carouselNav.find('.is-nav-selected').removeClass('is-nav-selected');
+        var $selected = $carouselNavCells.eq(flkty.selectedIndex).addClass('is-nav-selected');
+        var scrollY = $selected.position().top + $carouselNav.scrollTop() - (navHeight + navCellHeight) / 2;
+        $carouselNav.animate({
+            scrollTop: scrollY,
+        });
     });
 });
